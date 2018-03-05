d859b24c30b180e718e734a9488d11c0c5df8662
Johanes Jerrico Wijaya <johanes.wijaya@technosoft.com.sg>
2018-03-05 15:38:43 +0700

    change rollup into webpack umd

--- webpack.js
+++ webpack.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
 const root = path.resolve(__dirname,'src');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const DefinePlugin = webpack.DefinePlugin;
+const NoEmitOnErrorsPlugin = webpack.NoEmitOnErrorsPlugin;
 const ContextReplacementPlugin = webpack.ContextReplacementPlugin;
 
 module.exports = {
-
     output: {
-        path: path.resolve(__dirname,'release'),
-        filename: 'index.js',
-        publicPath: '/'
+        path: path.resolve(__dirname,'release','bundles'),
+        filename: 'store.umd.js',
+        library:'ngrx.store',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
     },
     entry: {
         main: path.resolve(__dirname,'index.ts')
@@ -18,11 +21,23 @@ module.exports = {
         extensions: ['.ts', '.js'],
         modules: [path.resolve(__dirname,"node_modules"),root]
     },
+    externals: {
+        'ng-metadata/core': 'ng.core',
+        '@ngrx/core': 'ngrx.core',
+        'rxjs/Observable': 'Rx',
+        'rxjs/BehaviorSubject': 'Rx',
+        'rxjs/Subscriber': 'Rx',
+        'rxjs/scheduler/queue': 'Rx.Scheduler',
+        'rxjs/operator/observeOn': 'Rx.Observable.prototype',
+        'rxjs/operator/scan': 'Rx.Observable.prototype',
+        'rxjs/operator/withLatestFrom': 'Rx.Observable'
+    },
     plugins: [
-        new ContextReplacementPlugin(
-            /ng-metadata/,
-            root
-        )
+        new NoEmitOnErrorsPlugin(),
+        new UglifyJsPlugin({ sourceMap: true }),
+        new DefinePlugin({
+            'ENV': { NODE_ENV: "'production'"}
+        })
     ],
     module: {
         rules: [
@@ -51,7 +66,11 @@ module.exports = {
     },
     node: {
         watch: false,
-    },
-    plugins: [
-    ]
+        global: true,
+        process: true,
+        crypto: 'empty',
+        module: false,
+        clearImmediate: false,
+        setImmediate: false
+    }
 }
